79ad8463b51753ca700c05594e5b7f558dafd257
ildrov <81831225+ildrov@users.noreply.github.com>
2023-05-14 19:35:08 +0700

    Add files via upload

---
+++ WebSite1 (front)/JavaScript.js
@@ -0,0 +1,10 @@
+﻿var toggleMenu = document.getElementById('toggle-menu');
+var menuItems = document.getElementById('menu-items');
+
+toggleMenu.addEventListener('click', function () {
+    if (menuItems.style.display === 'flex') {
+        menuItems.style.display = 'none';
+    } else {
+        menuItems.style.display = 'flex';
+    }
+});
